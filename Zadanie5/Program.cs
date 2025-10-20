c64fd3a5e33d18503f0ac5d227f878fdffe46f8a
Vlados <hanovvladik@mail.ru>
2025-10-20 10:29:49 +0300

    itog

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Zadanie5
+{
+    internal class Program
+    {
+        static async Task<int> GetDataAsync()
+        {
+            await Task.Delay(2000);
+            return 8;
+        }
+
+        static async Task Main()
+        {
+            Console.WriteLine("Ждем данные...");
+            int chislo = await GetDataAsync();
+            Console.WriteLine("Данные получены!");
+            Console.WriteLine($"Число: {chislo}");
+        }
+    }
+}
